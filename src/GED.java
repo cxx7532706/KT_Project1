427e6dae7f6d6d8188baca96171a608f5f92dcab
Xuanxi Chen <charles@Xuanxis-MacBook-Pro.local>
2017-04-06 14:28:39 +1000

    first commit

---
+++ src/GED.java
@@ -0,0 +1,149 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by charles on 31/3/17.
+ */
+public class GED {
+
+    private int Minimum(int a, int b, int c) {
+        int mi;
+
+        mi = a;
+        if (b < mi) {
+            mi = b;
+        }
+        if (c < mi) {
+            mi = c;
+        }
+        return mi;
+    }
+
+    private double Min2(double a, double b, double c) {
+        double mi;
+
+        mi = a;
+        if (b < mi) {
+            mi = b;
+        }
+        if (c < mi) {
+            mi = c;
+        }
+        return mi;
+    }
+
+    public int GEDCalculation1(String s, String t) {
+
+        int d[][]; // matrix
+        int n; // length of s
+        int m; // length of t
+        int i; // iterates through s
+        int j; // iterates through t
+        char s_i; // ith character of s
+        char t_j; // jth character of t
+        int cost; // cost
+        int replaceCost = 1;
+        int deleteCost = 1;
+        int insertCost = 1;
+
+
+        n = s.length();
+        m = t.length();
+        if (n == 0) {
+            return m;
+        }
+        if (m == 0) {
+            return n;
+        }
+        d = new int[n + 1][m + 1];
+
+
+        for (i = 0; i <= n; i++) {
+            d[i][0] = i;
+        }
+
+        for (j = 0; j <= m; j++) {
+            d[0][j] = j;
+        }
+
+
+        for (i = 1; i <= n; i++) {
+
+            s_i = s.charAt(i - 1);
+
+
+            for (j = 1; j <= m; j++) {
+
+                t_j = t.charAt(j - 1);
+
+                if ((s_i == t_j) || (t_j - s_i == 32)) {
+                    cost = 0;
+                } else {
+                    cost = replaceCost;
+                }
+                d[i][j] = Minimum(d[i - 1][j] + insertCost, d[i][j - 1] + deleteCost, d[i - 1][j - 1] + cost);
+            }
+        }
+        return d[n][m];
+
+    }
+
+    public double GEDCalculation2(String s, String t, double[][] table, double total) {
+
+        double d[][]; // matrix
+        int n; // length of s
+        int m; // length of t
+        int i; // iterates through s
+        int j; // iterates through t
+        char s_i; // ith character of s
+        char t_j; // jth character of t
+        double cost; // cost
+        double replaceCost = 2;
+        double deleteCost = 2;
+        double insertCost = 2;
+
+
+        n = s.length();
+        m = t.length();
+        if (n == 0) {
+            return m;
+        }
+        if (m == 0) {
+            return n;
+        }
+        d = new double[n + 1][m + 1];
+
+
+        for (i = 0; i <= n; i++) {
+            d[i][0] = i * insertCost;
+        }
+
+        for (j = 0; j <= m; j++) {
+            d[0][j] = j * deleteCost;
+        }
+
+        for (i = 1; i <= n; i++) {
+
+            s_i = s.charAt(i - 1);
+
+
+            for (j = 1; j <= m; j++) {
+
+                t_j = t.charAt(j - 1);
+
+                if ((s_i == t_j) || (t_j - s_i == 32)) {
+                    cost = 0;
+                }
+                else{
+                    cost = (1 - table[s_i][t_j]/total) * replaceCost;
+                }
+                d[i][j] = Min2(d[i - 1][j] + insertCost, d[i][j - 1] + deleteCost, d[i - 1][j - 1] + cost);
+            }
+        }
+        return d[n][m];
+
+    }
+
+}
+
---
+++ src/Main.java
@@ -0,0 +1,210 @@
+/**
+ * Created by charles on 31/3/17.
+ */
+
+import java.io.*;
+
+
+public class Main {
+
+    public static void GEDrun() {
+        try {
+
+            FileReader fr1 = new FileReader("src/train.txt");
+            BufferedReader br1 = new BufferedReader(fr1);
+            FileReader fr2 = new FileReader("src/names.txt");
+            BufferedReader br2 = new BufferedReader(fr2);
+            FileWriter fw = new FileWriter("src/output3.txt");
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            int len = (int) br2.lines().count();
+            br2.close();
+            fr2.close();
+
+            fr2 = new FileReader("src/names.txt");
+            br2 = new BufferedReader(fr2);
+
+            String[] names = new String[len + 1];
+            for (int i = 0; i < len; i++) {
+                names[i] = br2.readLine();
+            }
+            br2.close();
+            fr2.close();
+
+            FileReader fr = new FileReader("src/Analysis_table.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String line ;
+            double[][] table = new double[128][128];
+            for(int i =0; i< 127; i++)
+                for(int j = 0 ; j < 127; j++)
+                    table[i][j] = 0;
+            while((line = br.readLine())!= null){
+                String[] iterms = line.split(":");
+                double number = Double.parseDouble(iterms[1]);
+                String[] chars = iterms[0].split(" ");
+                table[chars[0].charAt(0)][chars[1].charAt(0)] = number;
+            }
+            br.close();
+            fr.close();
+
+            double total = 0;
+            for(int i =0; i< 127; i++)
+                for(int j = 0 ; j < 127; j++)
+                    total += table[i][j];
+
+            String s;
+            String[] predictedNames = new String[25910];
+            int numberOfPredictedNames = 0;
+            double minDistance = 100000;
+            GED ged = new GED();
+
+            while ((s = br1.readLine()) != null) {
+                String[] persianLatin = s.split("\\t");
+                StringBuilder sb = new StringBuilder();
+                sb.append(persianLatin[0]);
+                sb.append(" ");
+                for (int i = 0; i < len; i++) {
+                    double dis = ged.GEDCalculation2(persianLatin[0], names[i],table,total);  // GED Calculation
+                    if (dis < minDistance) {
+                        minDistance = dis;
+                        numberOfPredictedNames = 0;
+                        predictedNames[0] = names[i];
+                    } else if (dis == minDistance) {
+                        numberOfPredictedNames++;
+                        predictedNames[numberOfPredictedNames] = names[i];
+                    }
+                }
+                for (int i = 0; i <= numberOfPredictedNames; i++) {
+                    sb.append(predictedNames[i]);
+                    sb.append(" ");
+                }
+                sb.append("\n");
+                bw.write(sb.toString());
+                minDistance = 100000;
+                numberOfPredictedNames = 0;
+            }
+            bw.close();
+            fw.close();
+            br1.close();
+            fr1.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static double Evaluation(String output) {
+
+        try {
+
+            FileReader fr1 = new FileReader("src/train.txt");
+            BufferedReader br1 = new BufferedReader(fr1);
+            FileReader fr2 = new FileReader(output);
+            BufferedReader br2 = new BufferedReader(fr2);
+
+            FileWriter fw = new FileWriter("src/evaluation.txt");
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            String[][] source = new String[25910][];
+            String ss;
+            double correctNumber = 0, totalNumber = 0;
+            int len = 0;
+            while ((ss = br1.readLine()) != null) {
+                String[] lineData = ss.split("\\t");
+                source[len] = new String[lineData.length];
+                source[len] = lineData;
+                len++;
+            }
+            br1.close();
+            fr1.close();
+
+            while ((ss = br2.readLine()) != null) {
+                String[] lineData = ss.split(" ");
+                for (int i = 0; i < len; i++) {
+                    if (source[i][0].equals(lineData[0])) {
+                        for (int j = 1; j < lineData.length; j++) {
+                            if (source[i][1].equals(lineData[j])) {
+                                correctNumber = correctNumber + 1;
+                                StringBuilder sb = new StringBuilder();
+                                sb.append(lineData[0]);
+                                sb.append(" ");
+                                sb.append(lineData[j]);
+                                sb.append("\n");
+                                fw.write(sb.toString());
+                            }
+                            totalNumber = totalNumber + lineData.length - 1;
+                        }
+                    }
+                }
+
+            }
+            fw.close();
+            bw.close();
+            System.out.println(correctNumber);
+            System.out.println(totalNumber);
+            return correctNumber / totalNumber;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public static void Analyse(String file) {
+
+        try {
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            FileWriter fw = new FileWriter("src/Analysis_table.txt");
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            int[][] table = new int[128][128];
+            for (int i = 0; i < 127; i++)
+                for (int j = 0; j < 127; j++) {
+                    table[i][j] = 0;
+                }
+            String s;
+            while ((s = br.readLine()) != null) {
+                String[] data = s.split(" ");
+                if (data[0].length() == data[1].length()) {
+                    for (int i = 0; i < data[0].length(); i++) {
+                        if ((int) data[1].charAt(i) - (int) data[0].charAt(i) != 32) {
+                            table[(int) data[0].charAt(i)][(int) data[1].charAt(i)]++;
+                        }
+                    }
+                }
+            }
+            br.close();
+            fr.close();
+            for (int i = 0; i < 127; i++)
+                for (int j = 0; j < 127; j++)
+                    if (table[i][j] != 0) {
+                        StringBuilder sb = new StringBuilder();
+                        sb.append((char)i);
+                        sb.append(" ");
+                        sb.append((char)j);
+                        sb.append(":");
+                        sb.append(table[i][j]);
+                        sb.append("\n");
+                        fw.write(sb.toString());
+
+                    }
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    //     GEDrun();
+        double result = Evaluation("src/output3.txt");
+        System.out.println(result);
+
+        //double result = Evaluation("src/output1.txt");
+        //System.out.println(result);
+
+        //Analyse("src/evaluation.txt");
+
+    }
+}
